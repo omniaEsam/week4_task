21e77b01b0822c844a8d03b0f69afdb3b8f1bd93
omniaEsam <hossamelden544@gmail.com>
2024-03-28 02:26:13 +0200

    finish

--- src/store/index.js
+++ src/store/index.js
@@ -4,65 +4,64 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-    state: {
-      notes:[
-        {
-          id:1 ,
-          title:"learn html" , 
-          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, quas! Sed placeat provident vero, suscipit totam sapiente nulla ut aut. Dolore nam reiciendis maxime autem eum. At dicta quos doloremque! ",
-          complete:false
-         },
-        {
-          id:2 ,
-          title:"learn css" , 
-          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, quas! Sed placeat provident vero, suscipit totam sapiente nulla ut aut. Dolore nam reiciendis maxime autem eum. At dicta quos doloremque!",
-          complete:false
-        },
-      ]
-    },
-    getters: {
-      getNotes: state => state.notes
-    },
-    mutations: {
-      NotesFromLocalStorage:(state) =>{
-        const notes = localStorage.getItem('notes');
-        if (notes) {
-          state.notes = JSON.parse(notes);
-        }
-       },
-      addNotes: (state ,newNote )=>{
-        alert('Are you sure to add this note?')
-        if(newNote.title !== '' && newNote.description !== ''){
-          state.notes.push(newNote)
-        localStorage.setItem("notes" , JSON.stringify(state.notes))
-        }
-        else{
-          alert("Please fill in inputs field ")
-        }
-      }
-      ,
-      deleteNotes :(state , id)=>
+  state: {
+    notes: [
       {
-        alert('Are you sure about deleting this note?')
-        state.notes = state.notes.filter(note => note.id !== id)
-        localStorage.setItem("notes" , JSON.stringify(state.notes))
+        id: 1,
+        title: "learn html",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, quas! Sed placeat provident vero, suscipit totam sapiente nulla ut aut. Dolore nam reiciendis maxime autem eum. At dicta quos doloremque! ",
+        complete: false
       },
-      updateNote:(state , updateNote)=>{
-        alert('Are you sure about update this note?')
-        const index = state.notes.findIndex(note =>note.id === updateNote.id)
-        if(index !== -1){
-           state.notes.splice(index ,1,updateNote)
-        localStorage.setItem("notes" , JSON.stringify(state.notes))
-
-        }
+      {
+        id: 2,
+        title: "learn css",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, quas! Sed placeat provident vero, suscipit totam sapiente nulla ut aut. Dolore nam reiciendis maxime autem eum. At dicta quos doloremque!",
+        complete: false
+      },
+    ]
+  },
+  getters: {
+    getNotes: state => state.notes
+  },
+  mutations: {
+    NotesFromLocalStorage: (state) => {
+      const notes = localStorage.getItem('notes');
+      if (notes) {
+        state.notes = JSON.parse(notes);
       }
     },
-    actions: {
-      addingNotes: ({commit} , newNote)=>commit('addNotes' , newNote),
-      deleteingNotes:({commit} , id)=>commit('deleteNotes' , id),
-      updateingNote:({commit} , updateNote)=>commit('updateNote' , updateNote),
-      loadingNoteFromLocalStorge:({commit})=>commit('NotesFromLocalStorage')
+    addNotes: (state, newNote) => {
+      alert('Are you sure to add this note?')
+      if (newNote.title !== '' && newNote.description !== '') {
+        state.notes.push(newNote)
+        localStorage.setItem("notes", JSON.stringify(state.notes))
+      }
+      else {
+        alert("Please fill in inputs field ")
+      }
     }
-  })
-  store.dispatch('loadingNoteFromLocalStorge')
-  export default store;
+    ,
+    deleteNotes: (state, id) => {
+      alert('Are you sure about deleting this note?')
+      state.notes = state.notes.filter(note => note.id !== id)
+      localStorage.setItem("notes", JSON.stringify(state.notes))
+    },
+    updateNote: (state, updateNote) => {
+      alert('Are you sure about update this note?')
+      const index = state.notes.findIndex(note => note.id === updateNote.id)
+      if (index !== -1) {
+        state.notes.splice(index, 1, updateNote)
+        localStorage.setItem("notes", JSON.stringify(state.notes))
+
+      }
+    }
+  },
+  actions: {
+    addingNotes: ({ commit }, newNote) => commit('addNotes', newNote),
+    deleteingNotes: ({ commit }, id) => commit('deleteNotes', id),
+    updateingNote: ({ commit }, updateNote) => commit('updateNote', updateNote),
+    loadingNoteFromLocalStorge: ({ commit }) => commit('NotesFromLocalStorage')
+  }
+})
+store.dispatch('loadingNoteFromLocalStorge')
+export default store;
--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,87 +1,104 @@
 <template>
   <div>
-  <form class="form" @submit.prevent = "editMode ? update() : add()">
-        <input type="text" placeholder="title of notes" class="input" id="title" v-model='title' >
-        <textarea  placeholder="description" class="input" id="desc" v-model="description"></textarea>
-        <input type="submit" :value="editMode ? 'Update' : 'Create' " class="btn" >
+    <form class="form" @submit.prevent="editMode ? update() : add()">
+      <input
+        type="text"
+        placeholder="title of notes"
+        class="input"
+        id="title"
+        v-model="title"
+      />
+      <textarea
+        placeholder="description"
+        class="input"
+        id="desc"
+        v-model="description"
+      ></textarea>
+      <input
+        type="submit"
+        :value="editMode ? 'Update' : 'Create'"
+        class="btn"
+      />
     </form>
-  <table>
-     <tr v-for="note in notes" :key="note.id">
-      <td >{{note.title}}</td>
-      <td>{{note.description | truncate}}</td>
-      <td v-if="!note.complete">
-      <input type="checkbox" v-model="note.complete" id="check">
-      </td>
-      <td v-else><span>Completed</span></td>
-      <td> <button class="btn" id="update" @click="edit(note)">Edit</button></td>
-      <td> <button class="btn" id="delete" @click="remove(note.id)">Delete</button> </td>
-     </tr>
+    <table>
+      <tr v-for="note in notes" :key="note.id">
+        <td>{{ note.title }}</td>
+        <td>{{ note.description | truncate }}</td>
+        <td v-if="!note.complete">
+          <input type="checkbox" v-model="note.complete" id="check" />
+        </td>
+        <td v-else><span>Completed</span></td>
+        <td>
+          <button class="btn" id="update" @click="edit(note)">Edit</button>
+        </td>
+        <td>
+          <button class="btn" id="delete" @click="remove(note.id)">
+            Delete
+          </button>
+        </td>
+      </tr>
     </table>
   </div>
 </template>
 
 <script>
-import '../assets/main.css'
-import { mapGetters } from 'vuex'
+import "../assets/main.css";
+import { mapGetters } from "vuex";
 
 export default {
- name: 'Notes',
-data(){
-    return{
-         title: '',
-        description: '',
-        id:null,
-        editMode:false,
-    }
- },
- computed:{
-    ...mapGetters({
-      notes:'getNotes'
-    })
+  name: "Notes",
+  data() {
+    return {
+      title: "",
+      description: "",
+      id: null,
+      editMode: false,
+    };
   },
-  methods:{
-     add(){
-        this.$store.dispatch('addingNotes' , {
-            id:this.$store.getters.getNotes.length+1,
-            title: this.title ,
-            description:this.description
-        })
-         this.resetForm()
+  computed: {
+    ...mapGetters({
+      notes: "getNotes",
+    }),
+  },
+  methods: {
+    add() {
+      this.$store.dispatch("addingNotes", {
+        id: this.$store.getters.getNotes.length + 1,
+        title: this.title,
+        description: this.description,
+      });
+      this.resetForm();
     },
-    remove(id){
-        this.$store.dispatch('deleteingNotes', id)
+    remove(id) {
+      this.$store.dispatch("deleteingNotes", id);
     },
-    edit(note){
-          this.id = note.id
-          this.title = note.title
-          this.description = note.description
-          this.editMode = true
+    edit(note) {
+      this.id = note.id;
+      this.title = note.title;
+      this.description = note.description;
+      this.editMode = true;
     },
-    update(){
-         this.$store.dispatch('updateingNote' ,{
-            id: this.id,
-            title: this.title,
-            description: this.description
-         })
-         this.resetForm()
+    update() {
+      this.$store.dispatch("updateingNote", {
+        id: this.id,
+        title: this.title,
+        description: this.description,
+      });
+      this.resetForm();
     },
-    resetForm(){
-        this.editMode = false,
-        this.title = '',
-        this.description = '',
-        this.id = null
+    resetForm() {
+      (this.editMode = false),
+        (this.title = ""),
+        (this.description = ""),
+        (this.id = null);
     },
-    
-  }
-  ,
-  filters:{
-    truncate: function(value){
-        if(value !== ''){
-           return  value.substring(0,15) + '....'
-        }
-       
-    }
-  }
-
-}
+  },
+  filters: {
+    truncate: function (value) {
+      if (value !== "") {
+        return value.substring(0, 15) + "....";
+      }
+    },
+  },
+};
 </script>
